a4329b78d91d988ffdd91e55c7cd1eb3db432a09
Luís Piteira <lcpiteira@gmail.com>
2022-09-13 15:45:15 +0100

    Spring Initializr Project in java 11

---
+++ src/test/java/com/springlearning/guru/springmaster/SpringmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.guru.springmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
